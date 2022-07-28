82da179f8bf78e1982763a2dc7ace095d5acd594
Harshad K <108891281+harshad-diggibyte@users.noreply.github.com>
2022-07-28 22:20:40 +0530

    Add files via upload

---
+++ Spark Assignment 01/Main/Core/driver.py
@@ -0,0 +1,64 @@
+from pyspark import SparkConf
+from pyspark.sql import *
+from pyspark.sql import functions as F
+from utility import *
+
+"""
+This is driver class. starting point of your program
+"""
+import pyspark
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName('assignment_01').getOrCreate()
+spark
+
+user_data =[(101,"abc.123@gmail.com","hindi","mumbai"),
+            (102,"jhon@gmail.com","english","usa"),
+            (103,"madan.44@gmail.com","marathi","nagpur"),
+            (104,"local.88@outlook.com","tamil","chennai"),
+            (105,"sahil.55@gmail.com","english","usa"),
+            (106,"adi@gmail.com","hindi","nagpur"),
+            (107,"jason@gmail.com","marathi","mumbai"),
+            (108,"sohan@gmail.com","kannad","usa"),
+            (109,"case@outlook.com","tamil","mumbai"),
+            (110,"fury@gmail.com","hindi","nagpur")
+            ]
+
+user_schema = ["user_id","emailid","nativelanguage","location" ]
+
+userDF= spark.createDataFrame(data=user_data,schema=user_schema)
+
+display(userDF)
+
+transaction_data =[(3300101,1000001,101,700,"mouse"),
+                   (3300102,1000002,102,900,"keyboard"),
+                   (3300103,1000003,103,34000,"tv"),
+                   (3300104,1000004,101,35000,"fridge"),
+                   (3300105,1000005,105,55000,"sofa"),
+                   (3300106,1000006,106,100,"bed"),
+                   (3300107,1000007,105,66000,"laptop"),
+                   (3300108,1000008,108,20000,"phone"),
+                   (3300109,1000009,101,500,"speaker"),
+                   (3300110,1000010,102,1000,"chair")
+                   ]
+
+transaction_schema = ["transaction_id","product_id","userid","price","product_description" ]
+
+transactionDF= spark.createDataFrame(data=transaction_data,schema=transaction_schema)
+
+display(transactionDF)
+
+#joining the two data files
+df_join = userDF.join(transactionDF,userDF.user_id == transactionDF.userid,"full")
+display(df_join)
+
+#a)Count of unique locations where each product is sold.
+df_join.groupby('location').count().show()
+
+#b)	Find out products bought by each user.
+productcount=df_join.groupby(['user_id','product_description']).count()
+display(productcount.na.drop())
+
+#c)Total spending done by each user on each product.
+spendingbyuser=df_join.groupby(['user_id','product_description','price']).avg('price')
+display(spendingbyuser.na.drop())
+
